4665ceae566be9e0186065b992e4c274308064ca
benjGam <mecopilive@gmail.com>
2025-08-12 20:56:33 +0200

    tests: add 'BetterPathTesting' project

---
+++ BetterPathTesting/ExtensionsTests.cs
@@ -0,0 +1,15 @@
+﻿using BetterPaths;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BetterPathTesting
+{
+    [TestClass]
+    public class ExtensionsTests
+    {
+        [TestMethod]
+        public void IsValidPath_Test()
+        {
+            Assert.AreEqual(false, BetterPath.IsWellFormedPath("r"), "");
+        }
+    }
+}
---
+++ BetterPathTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BetterPathTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BetterPathTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("960e3f18-327a-411b-a5a1-22e96996fdcb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
