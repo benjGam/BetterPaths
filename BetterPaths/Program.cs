f142898464c8b91a84833291d3b95055e425cfcf
benjGam <mecopilive@gmail.com>
2025-08-12 19:37:35 +0200

    feat: add test form

--- BetterPaths/Program.cs
+++ BetterPaths/Program.cs
@@ -13,6 +13,7 @@ namespace BetterPaths
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TestForm());
         }
     }
 }
---
+++ BetterPaths/TestForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BetterPaths
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string pathToTest = @"rester";
+            MessageBox.Show($"pathToTest: {pathToTest} for 'IsWellFormedPath' is {BetterPath.IsWellFormedPath(pathToTest)}");
+            pathToTest = @"r:ster";
+            MessageBox.Show($"pathToTest: {pathToTest} for 'IsWellFormedPath' is {BetterPath.IsWellFormedPath(pathToTest)}");
+            pathToTest = @"rr\ster";
+            MessageBox.Show($"pathToTest: {pathToTest} for 'IsWellFormedPath' is {BetterPath.IsWellFormedPath(pathToTest)}");
+            pathToTest = @"r:\ter";
+            MessageBox.Show($"pathToTest: {pathToTest} for 'IsWellFormedPath' is {BetterPath.IsWellFormedPath(pathToTest)}");
+        }
+    }
+}
